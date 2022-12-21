696556d5d3638cd7a37a1253837befd08905af19
root <you@example.com>
2022-12-21 04:02:24 -0800

    update replace

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -6,17 +6,17 @@ $(document).ready(function() {
     // Listen for changes on each input checkbox tag
     $("input[type='checkbox']").change(function() {
       // Get the Amenity ID of the checkbox
-      var amenityName = $(this).attr("data-name");
+      var amenityName = $(this).attr("data-name").replace("/", "");
 
       // If the checkbox is checked, add the Amenity ID to the list
       if ($(this).is(":checked")) {
-        amenityNames.push(amenityName.replace("/", ""));
+        amenityNames.push(amenityName);
 
       }
       // If the checkbox is unchecked, remove the Amenity ID from the list
       else {
-        amenityNames = amenityNames.filter(function(id) {
-          return id !== amenityName;
+        amenityNames = amenityNames.filter(function(name) {
+          return name !== amenityName;
         });
       }
 
